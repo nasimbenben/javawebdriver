8ec2111d441ebee15f3acbbe24a93fb4f7066e9c
Tom <tom.wiggers@enexis.nl>
2018-10-10 13:46:35 +0200

    updated Feature file => "I" is "user"

--- src/test/java/StepDefs/CreteDeviceCasSetSIMSteps.java
+++ src/test/java/StepDefs/CreteDeviceCasSetSIMSteps.java
@@ -40,8 +40,8 @@ public class CreteDeviceCasSetSIMSteps {
     }
 
 
-    @Given("^That i am on logged in as user \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void that_i_am_on_logged_in_as_user_and_password(String user, String pass) {
+    @Given("^user is logged in as user \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_is_logged_in_as_user_and_password(String user, String pass) {
 
         this.loginPage.loginModule(this.loginPage.userName, user);
         this.loginPage.loginModule(this.loginPage.passWord, pass);
@@ -49,15 +49,15 @@ public class CreteDeviceCasSetSIMSteps {
 
     }
 
-    @When("^I  see my \"([^\"]*)\" overview page$")
-    public void i_see_my_overview_page(String pageHeader) throws Throwable {
+    @When("^user sees \"([^\"]*)\" overview page$")
+    public void user_sees_overview_page(String pageHeader) throws Throwable {
 
        this.dashBoardPage.visibilityOfElement(this.dashBoardPage.headerPage(pageHeader));
 
     }
 
-    @Then("^I click on \"([^\"]*)\" under the \"([^\"]*)\" main menu$")
-    public void i_click_on_under_the_main_menu(String subMenu, String mainMenu) throws Throwable {
+    @Then("^user clicks on \"([^\"]*)\" under the \"([^\"]*)\" main menu$")
+    public void user_clicks_on_under_the_main_menu(String subMenu, String mainMenu) throws Throwable {
 
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.openMainMenu());
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.mainMenu(mainMenu));
@@ -65,55 +65,55 @@ public class CreteDeviceCasSetSIMSteps {
     }
 
 
-    @Then("^I Select \"([^\"]*)\" under the \"([^\"]*)\" object$")
-    public void i_Select_under_the_object(String main, String sub) throws Throwable {
+    @Then("^user selects \"([^\"]*)\" under the \"([^\"]*)\" object$")
+    public void user_selects_under_the_object(String main, String sub) throws Throwable {
 
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.deviceTasks(main,sub));
     }
 
 
-    @Then("^I click on \"([^\"]*)\" under Device groups$")
-    public void i_click_on_under_Device_groups(String item) throws Throwable {
+    @Then("^user clicks on \"([^\"]*)\" under Device groups$")
+    public void user_clicks_on_under_Device_groups(String item) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
 
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.links(item));
     }
 
-    @Then("^I select \"([^\"]*)\" in a list overview$")
-    public void i_select_in_a_listoverview(String Device) throws Throwable {
+    @Then("^user selects \"([^\"]*)\" in a list overview$")
+    public void user_selects_in_a_listoverview(String Device) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
 
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.links(Device));
     }
 
-    @Then("^I click on \"([^\"]*)\"$")
-    public void i_click_on(String link) throws Throwable {
+    @Then("^user clicks on \"([^\"]*)\"$")
+    public void user_clicks_on(String link) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.links(link));
     }
 
 
-    @Then("^I click on \"([^\"]*)\" of the object menu$")
-    public void i_click_on_of_the_object_menu(String objectItem) throws Throwable {
+    @Then("^user clicks on \"([^\"]*)\" of the object menu$")
+    public void user_clicks_on_of_the_object_menu(String objectItem) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.objectMenu(objectItem));
     }
 
-    @Then("^I verify the \"([^\"]*)\" details and the \"([^\"]*)\" should be \"([^\"]*)\"$")
-    public void i_verify_the_details_and_the_should_be(String arg1, String arg2, String arg3) throws Throwable {
+    @Then("^user verifies the \"([^\"]*)\" details and the \"([^\"]*)\" should be \"([^\"]*)\"$")
+    public void user_verifies_the_details_and_the_should_be(String arg1, String arg2, String arg3) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
     }
 
-    @Then("^I verify  if \"([^\"]*)\" has the value \"([^\"]*)\"$")
-    public void i_verify_if_has_the_value(String object, String content) throws Throwable {
+    @Then("^user verifies  if \"([^\"]*)\" has the value \"([^\"]*)\"$")
+    public void user_verifies_if_has_the_value(String object, String content) throws Throwable {
 
         this.dashBoardPage.visibilityOfElement(this.dashBoardPage.deviceAttributesSingleElement(object));
         this.dashBoardPage.verifySingleElements(this.dashBoardPage.deviceAttributesSingleElement(object),content);
     }
 
 
-    @Then("^I verify if the objects has the correct values$")
-    public void i_verify_if_the_objects_has_the_correct_values(DataTable verifyElements) throws Throwable {
+    @Then("^user verifies if the objects has the correct values$")
+    public void user_verifies_if_the_objects_has_the_correct_values(DataTable verifyElements) throws Throwable {
 
         for (Map<String, String> data : verifyElements.asMaps(String.class, String.class)) {
             this.dashBoardPage.visibilityOfElement(this.dashBoardPage.deviceAttributesSingleElement(data.get("Object")));
@@ -123,14 +123,14 @@ public class CreteDeviceCasSetSIMSteps {
 
     }
 
-    @When("^I want to change the information i click on Edit \"([^\"]*)\" under the \"([^\"]*)\" button$")
-    public void i_want_to_change_the_information_i_click_on_Edit_under_the_button(String subButton,String button) throws Throwable {
+    @When("^user wants to change the information i click on Edit \"([^\"]*)\" under the \"([^\"]*)\" button$")
+    public void user_wants_to_change_the_information_i_click_on_Edit_under_the_button(String subButton,String button) throws Throwable {
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.deviceAttributesActionButton(button));
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.deviceAttributesActionSubButton(subButton));
     }
 
-    @Then("^I Change the Values from old value to new value$")
-    public void i_Change_the_Values_from_old_value_to_new_value(DataTable content) throws Throwable {
+    @Then("^user changes the Values from old value to new value$")
+    public void user_changes_the_Values_from_old_value_to_new_value(DataTable content) throws Throwable {
 
 
         this.dashBoardPage.visibilityOfElement(this.dashBoardPage.headerPage("Edit"));
@@ -145,14 +145,14 @@ public class CreteDeviceCasSetSIMSteps {
 
     }
 
-    @Then("^I want i click on the \"([^\"]*)\" button$")
-    public void i_Want_to_save_my_information_i_click_on_the_button(String buttonName) throws Throwable {
+    @Then("^user clicks on the \"([^\"]*)\" button$")
+    public void user_clicks_on_the_button(String buttonName) throws Throwable {
         this.dashBoardPage.waitAndClickOnElement(this.dashBoardPage.button(buttonName));
 
     }
 
-    @Then("^I verify if the below attributes has the correct values$")
-    public void i_verify_if_the_below_attributes_has_the_correct_values(DataTable verifyElements) throws Throwable {
+    @Then("^user verifies if the below attributes has the correct values$")
+    public void user_verifies_if_the_below_attributes_has_the_correct_values(DataTable verifyElements) throws Throwable {
 
         for (Map<String, String> data : verifyElements.asMaps(String.class, String.class)) {
             this.dashBoardPage.visibilityOfElement(this.dashBoardPage.deviceAttributesMultipleElement(data.get("object"),data.get("attribute")));
--- src/test/resources/CreateDeviceCasSetSIM.feature
+++ src/test/resources/CreateDeviceCasSetSIM.feature
@@ -2,30 +2,30 @@
 Feature: Verify device CAS set SIM information
 
   Scenario: As a end user i want to change the SIM details for a device
-    #Given That i am on logged in as user "Enexis_General" and password "DukeS01"
-    Given That i am on logged in as user "Enexis_NasimBenbouchta" and password "SzrhapMkP9"
-    When I  see my "Dashboard" overview page
-    Then I click on "Devices" under the "MultiSense" main menu
-    When I  see my "Devices" overview page
-    Then I Select "Device group" under the "Device groups" object
-    And I click on "Test Ruben" under Device groups
-    And I select "RonMeter" in a list overview
-    And I click on "Device attributes" of the object menu
-    When I  see my "Device attributes" overview page
-    Then I verify if the objects has the correct values
+    #Given user is logged in as user "Enexis_General" and password "DukeS01"
+    Given user is logged in as user "Enexis_NasimBenbouchta" and password "SzrhapMkP9"
+    When user sees "Dashboard" overview page
+    Then user clicks on "Devices" under the "MultiSense" main menu
+    When user sees "Devices" overview page
+    Then user selects "Device group" under the "Device groups" object
+    And user clicks on "Test Ruben" under Device groups
+    And user selects "RonMeter" in a list overview
+    And user clicks on "Device attributes" of the object menu
+    When user sees "Device attributes" overview page
+    Then user verifies if the objects has the correct values
       | Object                | Value                                |
       | Name                  | RonMeter                             |
       | MRID                  | 5db4c217-df3f-45af-8538-25fdc7a1a4d0 |
       | Device type           | Mx372 Test Devicetype                |
       | Serial number         | 987654                               |
       | Year of certification | 2018                                 |
-    #Then I verify  if "Name" has the value "RonMeter"
-    #And I verify  if "MRID" has the value "5db4c217-df3f-45af-8538-25fdc7a1a4d0"
-    #And I verify  if "Device type" has the value "Mx372 Test Devicetype"
-    #And I verify  if "Serial number" has the value "987654"
-    #And I verify  if "Year of certification" has the value "2019"
-    When I want to change the information i click on Edit "SIM" under the "Actions" button
-    Then I Change the Values from old value to new value
+    #Then user verifies if "Name" has the value "RonMeter"
+    #And user verifies if "MRID" has the value "5db4c217-df3f-45af-8538-25fdc7a1a4d0"
+    #And user verifies if "Device type" has the value "Mx372 Test Devicetype"
+    #And user verifies if "Serial number" has the value "987654"
+    #And user verifies if "Year of certification" has the value "2019"
+    When user wants to change the information i click on Edit "SIM" under the "Actions" button
+    Then user changes the Values from old value to new value
       |object   | value            |
       |Status   | Active           |
       |ICCID    | 55555555799      |
@@ -33,16 +33,16 @@ Feature: Verify device CAS set SIM information
       |Format   | Mini-SIM (2FF)   |
       |Batch    | 122288882        |
       |IMSI     | 2288888880       |
-    And I want i click on the "Save" button
-#Then I should see the "Edit SIM" overview page
-#And I change the SIM "Status" in the dropdown from "Created" to "Active"
-#And I change the SIM "ICCID" in the textfield from "555555557752" to "55555555799"
-#And I change the SIM "Provider" in the textfield from "KPN" to "Telfort"
-#And I change the SIM "Format" in the dropdown from "Full-size (1FF)" to "Mini-SIM (2FF)"
-#And I change the SIM "Batch ID" in the dropdown from "122288882" to "ç"
-#And I change the SIM "IMSI" in the dropdown from "8888888880" to "2288888880"
-    When I  see my "Device attributes" overview page
-    Then I verify if the below attributes has the correct values
+    And user clicks on the "Save" button
+#Then user sees the "Edit SIM" overview page
+#And user changes the SIM "Status" in the dropdown from "Created" to "Active"
+#And user changes the SIM "ICCID" in the textfield from "555555557752" to "55555555799"
+#And user changes the SIM "Provider" in the textfield from "KPN" to "Telfort"
+#And user changes the SIM "Format" in the dropdown from "Full-size (1FF)" to "Mini-SIM (2FF)"
+#And user changes the SIM "Batch ID" in the dropdown from "122288882" to "ç"
+#And user changes the SIM "IMSI" in the dropdown from "8888888880" to "2288888880"
+    When user sees "Device attributes" overview page
+    Then user verifies if the below attributes has the correct values
       |object|attribute|value            |
       |SIM   |Status   |Active           |
       |SIM   |ICCID    |55555555799      |
@@ -50,6 +50,6 @@ Feature: Verify device CAS set SIM information
       |SIM   |Format   |Mini-SIM (2FF)   |
       |SIM   |Batch ID |122288882        |
       |SIM   |IMSI     |2288888880       |
-#And I verify the "SIM" details and the "Status" should be "Active"
-#And I verify the "SIM" details and the "ICCID" should be "55555555799"
-#And I verify the "SIM" details and the "Provider" should be "Telfort"
+#And user verifies the "SIM" details and the "Status" should be "Active"
+#And user verifies the "SIM" details and the "ICCID" should be "55555555799"
+#And user verifies the "SIM" details and the "Provider" should be "Telfort"
